a979e0ca20e75ea10fbfa8223f51a82a3a2afebf
Davide <davidecavagnuolo@gmail.com>
2017-01-21 12:57:20 +0100

    Prototipo Onde Energetiche (Piano B)

---
+++ Piano B/Assets/GameManagerScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour {
+	public GameObject Onda1;
+	public GameObject Onda2;
+	public float ondaSpeed;
+	public GameObject endGamePanel;
+	public Text countdownText;
+	public Text winnerText;
+
+	private int phase = 0;
+	private bool player1Started;
+	private bool player2Started;
+
+	// Use this for initialization
+	void Start () {
+		endGamePanel.SetActive (false);
+		StartCoroutine (Countdown ());
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (phase == 1 && Onda1.transform.GetChild(0).GetComponent<OndaScript> ().collided) {
+			phase = 2;
+		}
+
+		if (phase == 1) {
+			Debug.Log (phase);
+			if (player1Started || Input.GetKeyDown (KeyCode.Q)) {
+				player1Started = true;
+				Vector3 s = Onda1.transform.localScale;
+				s.y += (ondaSpeed * Time.deltaTime);
+				Onda1.transform.localScale = s;
+			}
+			if (player2Started || Input.GetKeyDown (KeyCode.P)) {
+				player2Started = true;
+				Vector3 s = Onda2.transform.localScale;
+				s.y -= (ondaSpeed * Time.deltaTime);
+				Onda2.transform.localScale = s;
+			}
+		} else if (phase == 2) {
+			if (Input.GetKeyDown (KeyCode.Q)) {
+				Vector3 s = Onda1.transform.localScale;
+				s.y += 0.2f;
+				Onda1.transform.localScale = s;
+				s = Onda2.transform.localScale;
+				s.y += 0.2f;
+				Onda2.transform.localScale = s;
+			}
+			if (Input.GetKeyDown (KeyCode.P)) {
+				Vector3 s = Onda2.transform.localScale;
+				s.y -= 0.2f;
+				Onda2.transform.localScale = s;
+				s = Onda1.transform.localScale;
+				s.y -= 0.2f;
+				Onda1.transform.localScale = s;
+			}
+
+			if (Onda1.transform.localScale.y < 0) {
+				phase = 3;
+				winnerText.text = "Player 2 win!";
+				endGamePanel.SetActive (true);
+			} else if (Onda2.transform.localScale.y > 0) {
+				phase = 3;
+				winnerText.text = "Player 1 win!";
+				endGamePanel.SetActive (true);
+			}
+		}
+	}
+
+	IEnumerator Countdown() {
+		int count = 3;
+		do {
+			countdownText.text = count.ToString();
+			count--;
+			yield return new WaitForSeconds(1);
+		} while (count > 0);
+		phase = 1;
+		countdownText.enabled = false;
+	}
+
+	public void Restart() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+}
