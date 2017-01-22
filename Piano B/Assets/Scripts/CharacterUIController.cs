c5320b0a82a695ad43eb0d4ca74365eba025f354
Andrea Peretti <and.xet@gmail.com>
2017-01-22 14:13:10 +0100

    Added characters GUI

---
+++ Piano B/Assets/Scripts/CharacterUIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUIController : MonoBehaviour {
+    public GameObject firstRoundImage;
+    public GameObject secondRoundImage;
+    public PlayerPosition player;
+
+    // Use this for initialization
+    void Start () {
+        Refresh();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Refresh()
+    {
+        if (player == PlayerPosition.LEFT)
+        {
+            firstRoundImage.SetActive(MultiMatchController.Player1Wins >= 1);
+            secondRoundImage.SetActive(MultiMatchController.Player1Wins >= 2);
+        }
+        else if (player == PlayerPosition.RIGHT)
+        {
+            firstRoundImage.SetActive(MultiMatchController.Player2Wins >= 1);
+            secondRoundImage.SetActive(MultiMatchController.Player2Wins >= 2);
+        }
+    }
+}
